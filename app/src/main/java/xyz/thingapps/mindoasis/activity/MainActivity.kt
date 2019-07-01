704a1713e69426e730c825ea9348cfefd8d95d4c
ahndwon <gahlok24@gmail.com>
2019-07-01 13:34:22 +0900

    add settings layout

--- app/src/main/java/xyz/thingapps/mindoasis/activity/MainActivity.kt
+++ app/src/main/java/xyz/thingapps/mindoasis/activity/MainActivity.kt
@@ -1,12 +1,9 @@
 package xyz.thingapps.mindoasis.activity
 
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.debug
 import org.jetbrains.anko.toast
 import xyz.thingapps.mindoasis.R
 import xyz.thingapps.mindoasis.fragment.BookmarkFragment
@@ -15,47 +12,49 @@ import xyz.thingapps.mindoasis.fragment.SettingsFragment
 import xyz.thingapps.mindoasis.util.setStatusBarColor
 import xyz.thingapps.mindoasis.util.setStatusBarIconDark
 
-class MainActivity : AppCompatActivity(), SettingsFragment.OnFragmentInteractionListener, AnkoLogger {
+class MainActivity : AppCompatActivity(), AnkoLogger {
     private var backPressedTime = 0L
 
     companion object {
         const val FINISH_INTERVAL_TIME = 2000L
     }
 
-    override fun onFragmentInteraction(uri: Uri) {
-        debug("onFragmentInteraction")
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction()
-                .replace(R.id.mainContainer,
-                        HomeFragment(), HomeFragment::class.java.name)
-                .commit()
+            .replace(
+                R.id.fragmentContainer,
+                HomeFragment(), HomeFragment::class.java.name
+            )
+            .commit()
 
         val list = listOf(
-                R.id.navigation_home,
+            R.id.navigation_home,
             R.id.navigation_bookmark,
 //                R.id.navigation_share,
-                R.id.navigation_settings
+            R.id.navigation_settings
         )
 
         bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
             when (list.indexOf(menuItem.itemId)) {
                 0 -> {
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.mainContainer, HomeFragment(), HomeFragment::class.java.name)
-                            .commit()
+                        .replace(
+                            R.id.fragmentContainer,
+                            HomeFragment(),
+                            HomeFragment::class.java.name
+                        )
+                        .commit()
                     true
                 }
 
                 1 -> {
                     supportFragmentManager.beginTransaction()
-                            .replace(R.id.mainContainer, BookmarkFragment())
-//                            .addToBackStack(BookmarkFragment::class.java.name)
-                            .commit()
+                        .replace(R.id.fragmentContainer, BookmarkFragment())
+                        .commit()
                     true
                 }
 //
@@ -66,9 +65,8 @@ class MainActivity : AppCompatActivity(), SettingsFragment.OnFragmentInteraction
 
                 2 -> {
                     supportFragmentManager.beginTransaction()
-                            .replace(R.id.mainContainer, SettingsFragment())
-//                            .addToBackStack(SettingsFragment::class.java.name)
-                            .commit()
+                        .replace(R.id.fragmentContainer, SettingsFragment())
+                        .commit()
                     true
                 }
 
@@ -80,17 +78,6 @@ class MainActivity : AppCompatActivity(), SettingsFragment.OnFragmentInteraction
 
         setStatusBarColor(R.color.veryDarkNavy)
         setStatusBarIconDark(false)
-    }
-
-    override fun onAttachFragment(fragment: Fragment) {
-        if (fragment is SettingsFragment) {
-//            fragment.listener = object : SettingsFragment.OnFragmentInteractionListener {
-//                override fun onFragmentInteraction(uri: Uri) {
-//                    toast("Settings FragmentInteraction")
-//                }
-//            }
-            fragment.listener = this
-        }
     }
 
     override fun onBackPressed() {
